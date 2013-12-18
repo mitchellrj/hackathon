2757c9b7ae5d931120d87af13b4e0a8c92ab073e
Richard Mitchell <richard.j.mitchell@gmail.com>
2013-12-18 00:03:48 +0000

    Configure SQLAlchemy correctly.

--- hackathon/models/__init__.py
+++ hackathon/models/__init__.py
@@ -5,7 +5,14 @@ __author__ = 'Richard Mitchell <richard.mitchell@isotoma.com>'
 __docformat__ = 'restructuredtext en'
 __version__ = '$Revision$'[11:-2]
 
+import transaction
+
 from hackathon.models.base import Base
+from hackathon.models.entry import Entry
+from hackathon.models.hackathon import Hackathon
+from hackathon.models.submission import Submission
+from hackathon.models.task import Task
+from hackathon.models.user import User
 from hackathon.models.session import DBSession
 
 
@@ -13,3 +20,11 @@ def initialize_sql(engine):
     DBSession.configure(bind=engine)
     Base.metadata.bind = engine
     Base.metadata.create_all(engine)  # @UndefinedVariable
+
+    session = DBSession()
+    if not session.query(User).filter(User.email=='richard.mitchell@isotoma.com').first():
+        user = User(email='richard.mitchell@isotoma.com',
+                    name='Richard Mitchell')
+        user.password = 'foo'
+        session.add(user)
+        transaction.commit()
